6d40ec75d336c5c8cc9484ab8773118d2cbe1e49
Jan van der Meijde <j.van.der.meijde@boval.nl>
2025-04-02 14:23:29 +0200

    repo rename

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/wrknrs/', // Updated to match your repository name
+  base: '/wrknmrs/', // Updated to match your repository name
 })
